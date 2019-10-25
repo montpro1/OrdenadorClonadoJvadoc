a5e52c2b43056aedc8227331c855c83573e66e47
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-25 13:54:28 +0200

    javadoc

--- ordenador/pantalla.java
+++ ordenador/pantalla.java
@@ -15,22 +15,45 @@ public class pantalla {
     public pantalla(){
         
     }
+    /**
+     * en el metodo publico pantalla declaramos dos atributos: marca y pantalla.  
+     * @param marca De tipo string. Es una cadena caracteres
+     * @param pulgadas De tipo float. Con valor numerico que pueden ser decimales
+     */
     public pantalla(String marca, float pulgadas){
         this.marca=marca;
         this.pulgadas=pulgadas;
         
         
     }
+    /**
+     * 
+     * @param marca 
+     * es un metodo public que no devuelve nada y tiene el atributo marca de tipo string
+     */
     public void setMarca(String marca){
         this.marca=marca;
     }
+    /**
+     * es un metodo public que no devuelve nada y tiene el atributo pulgadas de tipo float
+     * @param pulgadas 
+     */
     public void setPulgadas(float pulgadas){
         this.pulgadas=pulgadas;
     }
+    
+    /**
+     * es un metodo public de tipo String 
+     * @return que devuelve  una cadena de caracteres de la clase marca
+     */
     public String getMarca(){
         return  marca;
         
     }
+    /**
+     * es un metodo public de tipo float
+     * @return que devuelve un valor numerico de la clase pulgadas
+     */
     public float getPulgadas(){
         return pulgadas;
     }
