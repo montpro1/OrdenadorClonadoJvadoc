f7f1ab7ceb4d0b56c311af0f32b280ae3ae690f2
yo <yo@192.168.1.37>
2019-11-28 01:32:32 +0100

    "Javadoc creado"

--- ordenador/Ordenador.java
+++ ordenador/Ordenador.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ordenador;
 
 import java.util.Scanner;
 
 /**
- *
+ *main class
  * @author vmontestovar
  */
 public class Ordenador {
@@ -17,7 +13,7 @@ public class Ordenador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+       
         
        Scanner sc=new Scanner(System.in);
        System.out.println("tipo de raton");
